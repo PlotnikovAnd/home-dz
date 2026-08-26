85190806f7d78f643135fbfa9d2bde9d58653e7d
a.plotnikov <xdernx@gmail.com>
2026-08-26 23:32:06 +0300

    add converter

---
+++ 1-converter/go.mod
@@ -0,0 +1,3 @@
+module demo/1-converter
+
+go 1.26.2
---
+++ 1-converter/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const usdToEurRate float64 = 0.86
+const usdToRubRate float64 = 84.37
+
+func main() {
+	fmt.Println("usd to eur const:", usdToEurRate)
+	fmt.Println("usd to rub const:", usdToRubRate)
+}
