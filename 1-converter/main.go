104acd394e9e7bf6c57d1494c687dd9f8b9e45a7
a.plotnikov <xdernx@gmail.com>
2026-08-27 19:42:48 +0300

    new commit

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,6 +6,20 @@ const eurFor1UsdRate float64 = 0.86
 const rubFor1UsdRate float64 = 84.37
 const eurToRubRate float64 = rubFor1UsdRate / eurFor1UsdRate
 
+func parseUserInput() float64 {
+	var input float64
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		fmt.Println("wrong input, error:", err)
+	}
+	return input
+}
+
+func calculateCurrency(amount float64, from string, to string) float64 {
+	return 1.0
+}
+
 func main() {
-	fmt.Println("rub for 1 eur:", eurToRubRate)
+	result := calculateCurrency(100, "usd", "rub")
+	fmt.Println("", result)
 }
